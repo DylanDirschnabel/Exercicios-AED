576f912e81a2ce9ce100e9307aeea67ddbb1dcb2
DylanDirschnabel <161781150+DylanDirschnabel@users.noreply.github.com>
2025-06-07 17:04:04 -0300

    Adicionado método de remoção

--- AEDLista11/src/main/java/questao2/aedlista11/ArvoreBinariaBusca.java
+++ AEDLista11/src/main/java/questao2/aedlista11/ArvoreBinariaBusca.java
@@ -58,6 +58,102 @@ public class ArvoreBinariaBusca<T extends Comparable<T>>  extends ArvoreBinariaA
         }
         
     }
+
+    public void retirar(T valor) {
+        NoArvoreBinaria p = super.getRaiz();
+        NoArvoreBinaria pai = null;
+        boolean filhoEsquerda = false;
+        
+        while(p != null && !p.getInfo().equals(valor)) {
+            pai = p;
+            if(valor.compareTo( (T) p.getInfo()) < 0) {
+                filhoEsquerda = true;
+                p = p.getEsquerda();
+            }
+            else {
+                filhoEsquerda = false;
+                p = p.getDireita();
+            }  
+        }
+        
+        if(p != null) {
+            
+            //caso 1
+            if(p.getDireita() == null && p.getEsquerda() == null) {
+                if(p.equals(super.getRaiz())) {
+                    super.setRaiz(null);
+                } else {
+                    if(filhoEsquerda == true) {
+                        pai.setEsquerda(null);
+                    } else {
+                        pai.setDireita(null);
+                    }
+                }
+            } else {
+                //caso 2
+                if(p.getDireita() == null) {
+                    if(super.getRaiz().equals(p)) {
+                        super.setRaiz(p.getEsquerda());
+                    } else {
+                        if(filhoEsquerda == true) {
+                            pai.setEsquerda(p.getEsquerda());
+                        } else {
+                            pai.setDireita(p.getEsquerda());
+                        }
+                    }
+                } else if (p.getEsquerda() == null) {
+                    if(super.getRaiz().equals(p)) {
+                        super.setRaiz(p.getDireita());
+                    } else {
+                        if(filhoEsquerda == true) {
+                            pai.setEsquerda(p.getDireita());
+                        } else {
+                            pai.setDireita(p.getDireita());
+                        }
+                    }
+                } else {
+                    //caso 3
+                    NoArvoreBinaria sucessor = extrairSucessor(p);
+                    
+                    if(p.equals(super.getRaiz())) {
+                        super.setRaiz(sucessor);
+                    } else {
+                        if(filhoEsquerda == true) {
+                            pai.setEsquerda(sucessor);
+                        } else {
+                            pai.setDireita(sucessor);
+                        }
+                    }
+                    sucessor.setEsquerda(p.getEsquerda());
+                }
+                
+                
+            }
+            
+            
+            
+            
+        }
+    }
+    
+    public NoArvoreBinaria extrairSucessor(NoArvoreBinaria p) {
+        NoArvoreBinaria atual = p.getDireita();
+        NoArvoreBinaria paiSucessor = p;
+        NoArvoreBinaria sucessor = p;
+        
+        while(atual != null) {
+            paiSucessor = sucessor;
+            sucessor = atual;
+            atual = atual.getEsquerda();
+        }
+        
+        if(sucessor != p.getDireita()) {
+            paiSucessor.setEsquerda(sucessor.getDireita());
+            sucessor.setDireita(p.getDireita());
+        }
+        
+        return sucessor;
+    }
     
     public NoArvoreBinaria<T> buscar(T info) {
         NoArvoreBinaria p = super.getRaiz();
